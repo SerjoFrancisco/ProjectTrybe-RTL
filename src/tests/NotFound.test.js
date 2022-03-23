26ecd6e6823cfd67c4e085a3879cf010f9640f0f
Sergio Francisco <spinheiro.lf@gmail.com>
2022-03-23 10:38:40 -0300

    fazendo os teste de página não encontrada

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('Test About.js', () => {
+  it('Tem a imagem e titulo certos', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(title).toBeInTheDocument();
+    const img = screen.getAllByRole('img');
+    expect(img[1]).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
