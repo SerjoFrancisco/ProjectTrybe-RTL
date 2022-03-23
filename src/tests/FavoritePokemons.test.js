e3501f8cd68984c4ae225a7a1b0fd5a2ad3c1929
Sergio Francisco <spinheiro.lf@gmail.com>
2022-03-22 21:56:23 -0300

    adicionando lista de favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+describe('Test About.js', () => {
+  it('Não retorna nada se não tiver favoritos ', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const title = screen.getByRole('heading', { name: /favorite pokémons/i });
+    expect(title).toBeInTheDocument();
+    const text = screen.getByText(/no favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+  it('retorna todos os pokemons favoritos', () => {
+    localStorage.setItem('favoritePokemonIds', '[25, 4]');
+    renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+    expect(screen.getByText('Charmander')).toBeInTheDocument();
+  });
+});
